303a1ef6b62e3419bc90f454f424b42c371a594c
RollingCC121 <daniel.gaviria0605@gmail.com>
2023-10-12 10:42:18 -0500

    actualizando script del bd

--- sql/db_script_examen_03.sql
+++ sql/db_script_examen_03.sql
@@ -4,21 +4,17 @@ CREATE TABLE programacion_autobuses (
   "horario_fk" int not null
 );
 ALTER TABLE programacion_autobuses ADD CONSTRAINT uk_id_programacion_autobuses UNIQUE ("id_programacion_autobuses");
-ALTER TABLE programacion_autobuses ADD CONSTRAINT uk_autobus_fk UNIQUE ("autobus_fk");
-ALTER TABLE programacion_autobuses ADD CONSTRAINT uk_horraio_fk UNIQUE ("horario_fk");
 
 comment on table programacion_autobuses is 'registro sobre la programacion de los autobuses';
 comment on column programacion_autobuses.id_programacion_autobuses is 'primary key de la tabla programacion_autobuses'; 
 comment on column programacion_autobuses.autobus_fk is 'fk asociada a la tabla autobus(id_autobus)';
 comment on column programacion_autobuses.horario_fk is 'fk asociada a la tabla horario(id_horario)';
 
-
 CREATE TABLE cargador (
   "id_cargador" serial PRIMARY KEY,
   "autobus_fk" int not null
 );
 alter table cargador add constraint uk_id_cargador unique ("id_cargador");
---alter table cargador add constraint uk_autobus_fk unique ("autobus_fk");
 
 comment on table cargador is 'registro sobre el funcionamiento del cargador';
 comment on column cargador.id_cargador is 'primary key de la tabla cargador';
@@ -30,8 +26,6 @@ CREATE TABLE horario (
   "hora_pico" varchar not null
 );
 alter table horario add constraint uk_id_horario unique ("id_horario");
-alter table horario add constraint uk_hora unique ("hora");
-alter table horario add constraint uk_hora_pico unique ("hora_pico");
 
 comment on table horario is 'registro del horario de funcionamiento de los servicios';
 comment on column horario.id_horario is 'primary key de la tabla horario';
@@ -45,9 +39,7 @@ CREATE TABLE autobus (
   "ruta" varchar not null
 );
 alter table autobus add constraint uk_id_autobus unique ("id_autobus");
-alter table autobus add constraint uk_placa unique ("placa");
-alter table autobus add constraint uk_marca unique ("marca");
-alter table autobus add constraint uk_ruta unique ("ruta");
+alter table autobus add constraint uk_id_placa unique ("placa");
 
 comment on table autobus is 'registro de autobuses';
 comment on column autobus.id_autobus is 'primary key de la tabla autobus';
@@ -61,8 +53,6 @@ CREATE TABLE programacion_cargadores (
   "autobus_fk" int not null
 );
 alter table programacion_cargadores add constraint uk_id_programacion_cargadores unique ("id_programacion_cargadores");
-alter table programacion_cargadores add constraint uk_horario_fk unique ("horario_fk");
---alter table programacion_cargadores add constraint uk_autobus_fk unique ("autobus_fk");
 
 comment on table programacion_cargadores is 'registro de la programacion de los cargadores';
 comment on column programacion_cargadores.id_programacion_cargadores is 'primary key de la tabla programacion_cargadores';
@@ -79,3 +69,31 @@ ALTER TABLE cargador ADD FOREIGN KEY ("autobus_fk") REFERENCES autobus ("id_auto
 ALTER TABLE programacion_autobuses ADD FOREIGN KEY ("autobus_fk") REFERENCES autobus ("id_autobus");
 
 ALTER TABLE programacion_autobuses ADD FOREIGN KEY ("horario_fk") REFERENCES horario ("id_horario");
+
+/*
+INSERT INTO horario (hora, hora_pico) VALUES
+    ('08:00:00', 'no'),
+    ('12:00:00', 'si'),
+    ('16:00:00', 'no');
+   
+INSERT INTO autobus (placa, marca, ruta) VALUES
+    ('ABC123', 'Toyota', 'Ruta1'),
+    ('XYZ789', 'BMW', 'Ruta2'),
+    ('DEF456', 'Mazda', 'Ruta3');
+
+INSERT INTO programacion_autobuses (autobus_fk, horario_fk) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+   
+INSERT INTO cargador (autobus_fk) VALUES
+    (1),
+    (2),
+    (3);
+   
+INSERT INTO programacion_cargadores (horario_fk, autobus_fk) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+*/
